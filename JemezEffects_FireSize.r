33a10f9b5506435dd125db49ab2785f1eed19135
Steve Bassett <sbassett@tnc.org>
2021-10-04 09:04:45 -0600

    data now included, additional summary stats

--- JemezEffects_FireSize.r
+++ JemezEffects_FireSize.r
@@ -2,7 +2,7 @@ require("scales")
 library(ggplot2)
 
 # load fire size list data
-FSIM_FireSizeListsAll_WithHeaders <- read.csv("I:/Projects/JemezTreatmentEffectiveness/Data/Interim/FireSimulation/FSimRuns/FSim_ProcessedManually_2021/FSIM_FireSizeListsAll_WithHeaders.csv", stringsAsFactors=TRUE)
+FSIM_FireSizeListsAll_WithHeaders <- read.csv("data/FSIM_FireSizeListsAll_WithHeaders.csv", stringsAsFactors=TRUE)
 
 # basic violin plot
 plotTest1 <- ggplot(FSIM_FireSizeListsAll_WithHeaders, aes(x=Scenario, y=SizeAc)) + geom_violin(fill="gray", trim=FALSE)
@@ -35,3 +35,37 @@ plotTest4 + stat_summary(fun=mean, geom="point", shape=18, size=5, color="red", 
 #jpeg(file="testPlot.jpg", width=1950, height=1200)
 #plotTest4 + theme_classic()
 #dev.off()
+
+### Annual Area Burned by Large Fires
+# Sum of all fire areas divided by 10,000 for each scenario.
+
+totalBurnedArea <- aggregate(SizeAc ~ Scenario, FSIM_FireSizeListsAll_WithHeaders, FUN = sum)
+totalBurnedArea
+
+totalBurnedArea$aveAnnualBurnedArea <- totalBurnedArea$SizeAc/10000
+totalBurnedArea
+
+### average annual probability of a fire over 100,000 acres 
+# Count of fires that grew to over 100,000 acres divided by 10,000 for each scenario.
+
+totalMegafire <- with( FSIM_FireSizeListsAll_WithHeaders[FSIM_FireSizeListsAll_WithHeaders$SizeAc >= 100000,], table(Scenario) )
+averageAnnualMegaFires <- totalMegafire/10000
+averageAnnualMegaFires
+
+### average fire size
+# Mean, median quartiles, etc of fire size for each scenario. [I'd expect to see the top quartile decrease faster than the mean or median.]
+fireSizeStats <- aggregate(SizeAc ~ Scenario, FSIM_FireSizeListsAll_WithHeaders, summary)
+fireSizeStats
+
+#function to find the mode from https://stackoverflow.com/a/25635740
+Mode <- function(x, na.rm = FALSE) {
+  if(na.rm){
+    x = x[!is.na(x)]
+  }
+  
+  ux <- unique(x)
+  return(ux[which.max(tabulate(match(x, ux)))])
+}
+
+#print the mode for each Scenario
+aggregate(SizeAc ~ Scenario, FSIM_FireSizeListsAll_WithHeaders, FUN = Mode)
